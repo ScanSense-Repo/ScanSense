5c5c0dcc5c0c2f900a0c3be01f021f90c33a3815
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2023-12-04 11:20:29 +0700

    Week_15 : fix profile screen

--- src/ScanSense/lib/ui/profile/profile_screen.dart
+++ src/ScanSense/lib/ui/profile/profile_screen.dart
@@ -21,17 +21,16 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
   @override
   void initState() {
-    cEmail.text = "ronny@gmail.com";
-    cNama.text = "Ronny";
-    cTelp.text = "0835627326372";
-    // TODO: implement initState
+    final auth = ref.read(authProvider);
+    cEmail.text = auth.auth?.user.email ?? "";
+    cNama.text = auth.auth?.user.name ?? "";
+    cTelp.text = auth.auth?.user.phoneNumber ?? "";
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final auth = ref.watch(authProvider);
-
     return Scaffold(
       appBar: AppBar(
         title: Align(
